0f2011f6765bb7b72300759e2a8927a005971716
Kiri Anthony <Kiriko.anthony@gmail.com>
2022-01-30 21:22:54 -0700

    starting save function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 
+var eventArray = [];
 var clickedTextArea = $("timeId");
-var clickedSaveButton = $("saveBtn");
 
 var displayTodaysDate = function() {
     var date = moment().format("dddd, MMM Do");
@@ -11,7 +11,7 @@ var displayTodaysDate = function() {
 var generateBlocks = function() {
     var currentHour = moment(new Date()).format("HH");
     for (var i = 0; i < 24; i++) {
-        var hourBlock = $("<div>").addClass("row");
+        var hourBlock = $("<div>").addClass("row").attr("id", i);
         if (i<10) {
             var time = "0"+i+":00";
         } else {
@@ -20,8 +20,9 @@ var generateBlocks = function() {
         var militaryTime = moment(time, "HH:mm").format("HH");
         var updatedTime = moment(time, "HH:mm").format("hh:mm A");
         var timeSection = $("<p>").addClass("hour time-block").text(updatedTime);
-        var textSection = $("<textArea>").addClass("textarea col-8").attr("id", i);
-        var saveButton = $("<button>").addClass("saveBtn").text("Save");
+        var textSection = $("<textArea>").addClass("textarea col-8").attr("id", i, "txtarea");
+        //console.log(textSection);
+        var saveButton = $("<button>").addClass("saveBtn").text("Save").attr("id", i);
         // create if statement to determine background color of calander text section
         if (currentHour - militaryTime > 0) {
             textSection.addClass("past");
@@ -38,3 +39,9 @@ var generateBlocks = function() {
 };
 
 generateBlocks();
+
+$(".saveBtn").click(function(event) {
+    var numClicked = $(this).attr("id");
+    
+});
+
