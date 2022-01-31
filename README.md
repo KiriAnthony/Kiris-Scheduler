4e941bcbe8f1f3ea7aec18a1b771c5938ea78c27
Kiri Anthony <Kiriko.anthony@gmail.com>
2022-01-30 19:52:56 -0700

    adding color coding to time blocks

--- README.md
+++ README.md
@@ -5,7 +5,7 @@
 ## Acceptance Criteria
 * When the calander is opened, the current day is displayed at the top
 * When scrolling down, standard hour time blocks are displayed on the page
-* The time blocks are color coded (red for past, yellow for present, green for future)
+* The time blocks are color coded (grey for past, red for present, green for future)
 * When a time block is clicked, an event can be entered
 * When the save button (for the individual time block) is clicked, the event is saved in local storage
 * When the page is refreshed, the saved events persist
--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,7 @@ var displayTodaysDate = function() {
 
 // function to generate blocks for hours 0 - 24
 var generateBlocks = function() {
+    var currentHour = moment(new Date()).format("HH");
     for (var i = 0; i < 24; i++) {
         var hourBlock = $("<div>").addClass("row");
         if (i<10) {
@@ -16,21 +17,19 @@ var generateBlocks = function() {
         } else {
             var time = i+":00";
         }
+        var militaryTime = moment(time, "HH:mm").format("HH");
         var updatedTime = moment(time, "HH:mm").format("hh:mm A");
         var timeSection = $("<p>").addClass("hour time-block").text(updatedTime);
-        var textSection = $("<textArea>").addClass("present textarea").attr("id", i);
-        var saveButton = $("<button>").addClass("saveBtn");
-        // update using moment.js to display i as an hour 1-12 with am/pm
-        // add id that corresponds to i
-        //textSection.textContent = "";
+        var textSection = $("<textArea>").addClass("textarea col-8").attr("id", i);
+        var saveButton = $("<button>").addClass("saveBtn").text("Save");
         // create if statement to determine background color of calander text section
-        // if (time is past) {
-        //     textSection.addClass("past");
-        // } else if (time is present) {
-        //     textSection.addClass("present");
-        // } else {
-        //     textSection.addClass("future");
-        // }
+        if (currentHour - militaryTime > 0) {
+            textSection.addClass("past");
+        } else if (currentHour - militaryTime < 0) {
+            textSection.addClass("future");
+        } else if (currentHour - militaryTime === 0){
+            textSection.addClass("present");
+        }
 
         hourBlock.append(timeSection, textSection, saveButton);
         $(".container").append(hourBlock);
